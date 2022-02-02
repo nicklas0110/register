4c878b72b49c325750f911d382fe26ab47c9addb
nicklas0110 <91014438+nicklas0110@users.noreply.github.com>
2022-02-02 10:05:10 +0100

    stuff

--- src/gui/Controller/SelectPositionModel.java
+++ src/gui/Controller/SelectPositionModel.java
@@ -1,4 +1,33 @@
 package gui.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SelectPositionModel {
+
+@FXML
+private Button buttonStudent;
+@FXML
+private Button buttonTeacher;
+
+    public void openStudentPage(ActionEvent actionEvent) throws IOException {
+        Stage switchScene = (Stage) buttonStudent.getScene().getWindow();
+        Parent parent = FXMLLoader.load(getClass().getResource("../View/2.1_StudentLogin.fxml"));
+        Scene scene = new Scene(parent);
+        switchScene.setScene(scene);
+    }
+
+    public void openTeacherPage(ActionEvent actionEvent) throws IOException {
+        Stage switchScene = (Stage) buttonTeacher.getScene().getWindow();
+        Parent parent = FXMLLoader.load(getClass().getResource("../View/2_TeacherLogin.fxml"));
+        Scene scene = new Scene(parent);
+        switchScene.setScene(scene);
+    }
 }
