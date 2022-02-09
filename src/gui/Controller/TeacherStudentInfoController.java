cd583f58ff0d4e97add5c80c6fcd6829960ac665
FrederikReigstad <Frederik9876@gmail.com>
2022-02-09 13:18:35 +0100

    Teacher student info view

---
+++ src/gui/Controller/TeacherStudentInfoController.java
@@ -0,0 +1,28 @@
+package gui.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TeacherStudentInfoController {
+    @FXML
+    public Button mainVeiwBtn;
+    @FXML
+    public Button logoutBtThView;
+
+    public void logoutBtStView(ActionEvent actionEvent) throws IOException {
+        Stage switchScene = (Stage) logoutBtThView.getScene().getWindow();
+        Parent parent = FXMLLoader.load(getClass().getResource("../View/Login.fxml"));
+        Scene scene = new Scene(parent);
+        switchScene.setScene(scene);
+    }
+
+    public void backMainVeiwBtn(ActionEvent actionEvent) {
+    }
+}
