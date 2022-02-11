1073d1010589c284147a636245ce7a121949653b
MostafaHeidari <70130828+MostafaHeidari@users.noreply.github.com>
2022-02-11 15:45:24 +0100

    navngivning af buttons

--- src/gui/Controller/LoginController.java
+++ src/gui/Controller/LoginController.java
@@ -13,20 +13,20 @@ import java.io.IOException;
 public class LoginController {
 
 @FXML
-private Button loginBt;
+private Button loginSudentBt;
 @FXML
-private Button loginBtT;
+private Button loginTeacherBt;
 
 
-    public void loginBt(ActionEvent actionEvent) throws IOException {
-        Stage switchScene = (Stage) loginBt.getScene().getWindow();
+    public void loginSudentBt(ActionEvent actionEvent) throws IOException {
+        Stage switchScene = (Stage) loginSudentBt.getScene().getWindow();
         Parent parent = FXMLLoader.load(getClass().getResource("../View/Student/StudentHomePage.fxml"));
         Scene scene = new Scene(parent);
         switchScene.setScene(scene);
     }
 
-    public void loginBtT(ActionEvent actionEvent) throws IOException {
-        Stage switchScene = (Stage) loginBtT.getScene().getWindow();
+    public void loginTeacherBt(ActionEvent actionEvent) throws IOException {
+        Stage switchScene = (Stage) loginTeacherBt.getScene().getWindow();
         Parent parent = FXMLLoader.load(getClass().getResource("../View/Teacher/TeacherHomePage.fxml"));
         Scene scene = new Scene(parent);
         switchScene.setScene(scene);
