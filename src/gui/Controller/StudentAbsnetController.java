2366b0f7e6df616a55569920968c8a9f49e333e2
nicklas0110 <91014438+nicklas0110@users.noreply.github.com>
2022-02-16 11:12:28 +0100

    dialog window

--- src/gui/Controller/StudentAbsnetController.java
+++
@@ -1,4 +0,0 @@
-package gui.Controller;
-
-public class StudentAbsnetController {
-}
---
+++ src/gui/Controller/yesANDno.java
@@ -0,0 +1,23 @@
+package gui.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class yesANDno {
+
+
+    @FXML
+    private Button yes;
+    @FXML
+    private Button no;
+
+    public void yes(ActionEvent actionEvent) {
+        //Stage stage = (Stage) no.getScene().getWindow();
+    }
+
+    public void no(ActionEvent actionEvent) {
+    }
+}
