814db0aff0e4cd781d0a2ab967ed8ca68aa3e561
MostafaHeidari <70130828+MostafaHeidari@users.noreply.github.com>
2022-02-16 13:15:02 +0100

    et random BarChart blev lavet i StudentViewPageController.java. den viser et tilfælde chart

--- src/gui/Controller/StudentViewPageController.java
+++ src/gui/Controller/StudentViewPageController.java
@@ -5,16 +5,23 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.Axis;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class StudentViewPageController {
     @FXML
     public Button mainVeiwBtn;
     @FXML
     private Button logoutBtStView;
+    @FXML
+    private BarChart<String, Number> chart;
 
     public void logoutBtStView(ActionEvent actionEvent) throws IOException {
         Stage switchScene = (Stage) logoutBtStView.getScene().getWindow();
@@ -29,4 +36,33 @@ public class StudentViewPageController {
         Scene scene = new Scene(parent);
         switchScene.setScene(scene);
     }
+
+
+
+
+    public void drawChart(ActionEvent actionEvent) {
+        Axis<String> xAxis = chart.getXAxis();
+        xAxis.setLabel("Day");
+
+        /*Axis<Number> yAxis = chart.getYAxis();
+        yAxis.setLabel("Amount");
+
+         */
+
+
+        Random r = new Random();
+        for (int i = 0; i < 6; i++) {
+            XYChart.Series<String, Number> series = new XYChart.Series<>();
+            series.setName("Week " + (i + 4));
+
+            series.getData().add(new XYChart.Data<>("Monday", r.nextInt(10) + 10));
+            series.getData().add(new XYChart.Data<>("Tuesday", r.nextInt(20) + 23));
+            series.getData().add(new XYChart.Data<>("Wednesday", r.nextInt(40) + 45));
+            series.getData().add(new XYChart.Data<>("Thursday", r.nextInt(50) + 55));
+            series.getData().add(new XYChart.Data<>("Friday", r.nextInt(70) + 75));
+            chart.getData().add(series);
+        }
+
+    }
+
 }
